11ee134a1b1ec1928ce3e42c0904b99a820e686d
Omprakash <tjs4@Omprakashs-MacBook-Pro.local>
2021-09-24 17:18:11 +0530

    done

--- album/src/App.js
+++ album/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import Album from "./components/Album";
+import Albums from "./components/Albums/Albums";
 
 function App() {
   return <div className="App">
-    <Album/>
+    <Albums/>
   </div>;
 }
 
--- album/src/components/Data.js
+++ album/src/components/Data.js
@@ -1,44 +1,74 @@
 export const data = [
   {
     id: 1,
-    album: "abc1",
-    image: "https://picsum.photos/400",
-    title: "title",
-    song: "https://youtu.be/7ydfq77_dx8",
+    artist: "Kishore Kumar",
+    album: "Mr.X In Bombay",
+    image:
+      "https://i.ytimg.com/an_webp/ASkYJ0J5wk8/mqdefault_6s.webp?du=3000&sqp=COyetooG&rs=AOn4CLDZJPKVlIRhgPCenV3Ebx0qXWq8hg",
+    title: "Mere Mehboob Qayamat Hogi",
+    song: "https://youtu.be/OyrfNNL7gao",
   },
   {
     id: 2,
-    album: "abc2",
-    image: "https://picsum.photos/401",
-    title: "title",
-    song: "https://youtu.be/7ydfq77_dx8",
+    artist: "Rishi Kapur",
+    album: "Deewana",
+    image:
+      "https://i.ytimg.com/an_webp/lFdSi01tpYM/mqdefault_6s.webp?du=3000&sqp=CNG9tooG&rs=AOn4CLCPwIEPcUONq303LS8yDnysVDP2TA",
+    title: "Sochenge tumhe pyar",
+    song: "https://youtu.be/lFdSi01tpYM",
   },
   {
     id: 3,
-    album: "abc3",
-    image: "https://picsum.photos/402",
-    title: "title",
-    song: "https://youtu.be/7ydfq77_dx8",
+    artist: "Mannara, Karanvir",
+    album: "Zid",
+    image:
+      "https://i.ytimg.com/an_webp/oNA5wTiNmS8/mqdefault_6s.webp?du=3000&sqp=CIWztooG&rs=AOn4CLCZ92UKEI17K_VNkDktd2q5TrYZGw",
+    title: "Tu Zaroori ",
+    song: "https://youtu.be/oNA5wTiNmS8",
   },
   {
     id: 4,
-    album: "abc4",
-    image: "https://picsum.photos/403",
-    title: "title",
-    song: "https://youtu.be/7ydfq77_dx8",
+    artist: "K.K. ",
+    album: "The Train",
+    image:
+      "https://i.ytimg.com/an_webp/UlacMvx_VYk/mqdefault_6s.webp?du=3000&sqp=CMi-tooG&rs=AOn4CLDjIheT-XbdU-dhqw54OGsqZAkQLQ",
+    title: "Beete Lamhe",
+    song: "https://youtu.be/UlacMvx_VYk",
   },
   {
     id: 5,
-    album: "abc5",
-    image: "https://picsum.photos/404",
-    title: "title",
-    song: "https://youtu.be/7ydfq77_dx8",
+    artist: "Akshaye Khanna & Urvashi Sharma",
+    album: "Naqaab",
+    image:
+      "https://i.ytimg.com/an_webp/ASkYJ0J5wk8/mqdefault_6s.webp?du=3000&sqp=COyetooG&rs=AOn4CLDZJPKVlIRhgPCenV3Ebx0qXWq8hg",
+    title: "Ek Din Teri Raahon Mein",
+    song: "https://youtu.be/8Fz2GXx9Qsw",
   },
   {
     id: 6,
-    album: "abc6",
-    image: "https://picsum.photos/405",
-    title: "title",
-    song: "https://youtu.be/7ydfq77_dx8",
+    artist: "Rishi Kapur",
+    album: "Deewana",
+    image:
+      "https://i.ytimg.com/an_webp/lFdSi01tpYM/mqdefault_6s.webp?du=3000&sqp=CNG9tooG&rs=AOn4CLCPwIEPcUONq303LS8yDnysVDP2TA",
+    title: "Sochenge tumhe pyar",
+    song: "https://youtu.be/lFdSi01tpYM",
+  },
+  {
+    id: 7,
+    artist: "Mannara, Karanvir",
+    album: "Zid",
+    image:
+      "https://i.ytimg.com/an_webp/oNA5wTiNmS8/mqdefault_6s.webp?du=3000&sqp=CIWztooG&rs=AOn4CLCZ92UKEI17K_VNkDktd2q5TrYZGw",
+    title: "Tu Zaroori ",
+    song: "https://youtu.be/oNA5wTiNmS8",
+  },
+  {
+    id: 8,
+    artist: "K.K. ",
+    album: "The Train",
+    image:
+      "https://i.ytimg.com/an_webp/UlacMvx_VYk/mqdefault_6s.webp?du=3000&sqp=CMi-tooG&rs=AOn4CLDjIheT-XbdU-dhqw54OGsqZAkQLQ",
+    title: "Beete Lamhe",
+    song: "https://youtu.be/UlacMvx_VYk",
   },
 ];
